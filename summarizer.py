6a0663753d2e7f09ec3c6f78a2e1af9469f1d9d2
Zizhen Song <zizhensong@gmail.com>
2020-10-17 14:51:18 -0700

    edit summarizer to return summary string

--- summarizer.py
+++ summarizer.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def summary_from_url(url, sentences):
   f = open("key.txt", "r")
@@ -21,10 +22,12 @@ def summary_from_text(txt, sentences):
 def summary(querystring):
   url = "https://api.meaningcloud.com/summarization-1.0"
   response = requests.request("Post", url, headers=None, params=querystring)
-  return(response.text)
+  data = json.loads(response.text)
 
-# Uncomment for sample usage
-#
+  return(data["summary"])
+
+#Uncomment for sample usage
+
 # txt = """
 # Joe Biden believes to his core that there’s no greater economic engine in the world than the hard work and ingenuity of the American people. Nobody has more respect for the working women and men who get up every day to build and sustain this country, or more confidence that they can meet the challenges we face.
 # Make no mistake: America has been knocked down. The unemployment rate is higher than it was in the Great Recession. Millions have lost jobs, hours, pay, health care, or the small business they started, through no fault of their own.
