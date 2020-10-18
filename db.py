ece6e5fb2684cf232545ef7e770e8173f163d7a5
Samuel Berensohn <samberensohn@gmail.com>
2020-10-18 00:16:25 -0700

    Final

--- db.py
+++ db.py
@@ -12,10 +12,11 @@ db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 def open_connection():
     unix_socket = '/cloudsql/{}'.format(db_connection_name)
     try:
-      conn = pymysql.connect(user=db_user, password=db_password,
-                             unix_socket=unix_socket, db=db_name,
-                             cursorclass=pymysql.cursors.DictCursor
-                             )
+      if os.environ.get('GAE_ENV') == 'standard':
+        conn = pymysql.connect(user=db_user, password=db_password,
+                               unix_socket=unix_socket, db=db_name,
+                               cursorclass=pymysql.cursors.DictCursor
+                               )
     except pymysql.MySQLError as e:
         print(e)
 
@@ -24,10 +25,11 @@ def open_connection():
 def get_summary(candidate, issue, length):
   conn = open_connection()
   with conn.cursor() as cursor:
-    query = '''SELECT s.{length}_summary, s.source, s.scrape_date
+    query = '''SELECT s.{}_summary, s.source, s.scrape_date
                FROM candidates as c, candidate_content as s, issues as i
                WHERE c.id = s.candidate_id AND s.issue_id = i.id
-                     AND i.name = '{issue}' AND c.name = '{candidate}';'''
+                     AND i.name = '{}' AND c.name = '{}';'''.format(length, issue, candidate)
+    print(query)
     result = cursor.execute(query)
     content = cursor.fetchall()
     if result == 1:
@@ -36,3 +38,19 @@ def get_summary(candidate, issue, length):
       retreived_content = 'No Content For Specific Individiual'
   conn.close()
   return retreived_content
+
+def post_summary(content_id, candidate_id, issue_id, short_summary, med_summary, long_summary, source):
+  conn = open_connection()
+  with conn.cursor() as cursor:
+    cursor.execute('INSERT INTO candidate_content VALUES ({}, \'{}\', \'{}\', \'{}\', {}, {}, \'{}\', CURDATE())'.format(
+      content_id,
+      short_summary,
+      med_summary,
+      long_summary,
+      issue_id,
+      candidate_id,
+      source
+      )
+    )
+  conn.commit()
+  conn.close()
--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask, jsonify
-from db import get_summary
+from flask import Flask, jsonify, request
+from db import get_summary, post_summary
 
 app = Flask(__name__)
 
+ISSUES = {
+  "Covid": 1,
+  "Climate Change": 2,
+  "Immigration": 3,
+  "Clean Energy": 4,
+  "Foreign Policy": 5,
+  "Reopening Schools": 6,
+  "National Security": 7,
+  "Law Enforcement": 8,
+  "Senior Citizens": 9,
+  "Education": 10,
+  "Economy and Jobs": 11,
+  "Veterans": 12
+}
+
+CANDIDATES = {
+  "Trump": 1,
+  "Biden": 2
+}
+                          ## Donald%20Trump       Covid          med
 @app.route('/get-summary/<string:candidate>/<string:issue>/<string:length>')
-def summary_route(candidate, issue, length):
+def get_summary_route(candidate, issue, length):
   return get_summary(candidate, issue, length)
+
+@app.route('/post-summary/<string:candidate>/<string:issue>/<string:content_id>', methods=["POST"])
+def post_summary_route(content_id, candidate, issue):
+  data = request.get_json()
+  print(data)
+  post_summary(content_id, CANDIDATES[candidate], ISSUES[issue], data['short'], data['medium'], data['long'], data['url'])
+  return "Completed"
 
 if __name__ == '__main__':
   app.run()
